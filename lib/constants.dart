574a296f076b215d18ee7c56faa040dc6ef52c66
MohamedAshrafz <69872998+MohamedAshrafz@users.noreply.github.com>
2023-10-01 23:15:46 +0300

    make the dice a little bit bigger

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const double diceIconSize = 200.0;
+const double diceIconSize = 240.0;
 const appColorGradientList = [
   Color.fromARGB(255, 163, 34, 219),
   Color.fromARGB(255, 109, 66, 219),
